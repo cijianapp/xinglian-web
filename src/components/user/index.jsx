4d7bcfac859a56213eac2514a633bd790594a740
wenyuyang <wenyuyang@gwu.edu>
2020-07-04 00:10:39 -0400

    logout

--- src/components/user/index.jsx
+++ src/components/user/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./styles.module.css";
+import ReactTooltip from "react-tooltip";
 import { useSelector } from "react-redux";
 
 import { Avatar } from "../collections";
@@ -9,11 +10,41 @@ export const User = () => {
   const info = useSelector(infoSelector);
 
   return (
-    <div className={styles.user}>
-      <Avatar url={info.avatar} size={32}></Avatar>
-      <div style={{ paddingLeft: "5px", fontSize: "14px" }}>
-        {info.username}
+
+    <div>
+      <div
+        className={styles.user}
+        data-tip
+        data-for="user-details"
+        data-event="click"
+      >
+        <Avatar url={info.avatar} size={32}></Avatar>
+        <div style={{ paddingLeft: "5px", fontSize: "14px" }}>
+          {info.username}
+        </div>
+
+
       </div>
+
+      <ReactTooltip
+        id="user-details"
+        place="bottom"
+        type="dark"
+        effect="solid"
+        globalEventOff="click"
+        eventOff="dbclick"
+        clickable={true}
+        className={styles.tooltips}
+        offset={{ top: 5 }}
+      >
+        <div style={{ color: "#000", padding: "10px", cursor: "pointer" }}>
+          设置
+        </div>
+        <div style={{ color: "#000", padding: "10px", cursor: "pointer" }}>
+          登出
+        </div>
+      </ReactTooltip>
     </div>
+
   );
 };
